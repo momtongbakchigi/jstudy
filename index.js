e3615c980e4fc6a838b0a7c030675ce2facc3f02
unknown <ehddn>
2021-07-22 15:50:12 +0900

    jstudy추가

---
+++ index.js
@@ -0,0 +1,29 @@
+// var string = 'hello world';
+// var search = 'l';
+// var count = 0;
+
+// const regexp = new RegExp(search,'g');
+// console.log(string.match(regexp).length);
+
+// var elem = null;
+// var value = elem && elem.value;
+// console.log(value);
+
+
+// function getStringLength(str) {
+//     str = str || '' ;
+//     console.log(str.length);
+//     return str.length;
+// }
+
+// getStringLength();
+// getStringLength('hi');
+
+// function getStringLengths(str='') {
+//     console.log(str.length);
+//     return str.length;
+// }
+
+// getStringLengths();
+// getStringLengths('hi');
+
